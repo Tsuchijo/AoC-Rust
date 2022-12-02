e604a5934f97dfa56fd692681e9f058a76036b9e
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2022-12-02 01:49:32 -0800

    Completed day 1 part 2

--- day1/day1/src/main.rs
+++ day1/day1/src/main.rs
@@ -1,8 +1,12 @@
 use std::fs;
 
 fn main() {
+   day1();
+}
+
+fn day1(){
     let file_path = "input.txt";
-    let mut max: u32 = 0;
+    let mut max_list: Vec<u32> = Vec::new();
     let mut running: u32 = 0;
     println!("In file {}", file_path);
     let contents = fs::read_to_string(file_path)
@@ -10,14 +14,13 @@ fn main() {
     let split = contents.split('\n');
     for val in split {
         if val.eq(&String::from("")){
-            if max < running {
-                max = running;
-                println!("new Max: {}", running);
-            }
+            max_list.push(running);
             running = 0;   
         }
         else {
             running += val.parse::<u32>().unwrap();
         }
     }
+    max_list.sort_by(|a, b| b.cmp(a)); 
+    println!("The top three are: {}", max_list[0] + max_list[1] + max_list[2]);
 }
